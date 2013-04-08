4dbdc9e73ff71a522f212809f2db97dbd84829e0
Niall O'Brien <me@niallobrien.me>
2013-04-08 12:23:28 +0100

    Updated db info for prod.

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'flockapp.mysql.eu1.frbit.com',
+			'host'      => 'localhost',
 			'database'  => 'flockapp',
-			'username'  => 'flockapp',
-			'password'  => 'VHLJQ4tOJkFCqoc_',
+			'username'  => 'root',
+			'password'  => 'Beforethefl00d054',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
